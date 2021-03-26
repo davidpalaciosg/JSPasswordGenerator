c733208b7ca7e2b836aaafb87b8245c91ca30dea
David Palacios <paladavid@hotmail.com>
2021-03-26 14:35:48 -0500

    password feature added

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    const numbers = "0123456789";
+    const lowercase = "abcdefghijklmnopqrstuwvxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const special = "!@&#)(%^*?";
+
+
+    $("#send-button").click(function (e) {
+        e.preventDefault();
+        var string = "";
+        let numchecked = false;
+        let lowerchecked = false;
+        let upperchecked = false;
+        let specialchecked = false;
+
+
+        if ($('#numbers').is(":checked"))
+            numchecked = true;
+
+        if ($('#lowercase').is(":checked"))
+            lowerchecked = true;
+
+        if ($('#uppercase').is(":checked"))
+            upperchecked = true;
+
+        if ($('#special').is(":checked"))
+            specialchecked = true;
+
+        //If any option is checked
+        if (numchecked || lowerchecked || upperchecked || specialchecked) {
+            //Build string
+            string = buildString(numchecked, lowerchecked, upperchecked, specialchecked);
+
+            let quantity = $("#quantity").val();
+            if (quantity < 8)
+                quantity = 8;
+
+            //Build password
+            let password = buildPassword(string, quantity);
+            //Show password
+            $("#password").val(password);
+        }
+    });
+
+    function buildString(numchecked, lowerchecked, upperchecked, specialchecked) {
+
+        let answer = "";
+
+        if (numchecked)
+            answer += numbers;
+
+        if (lowerchecked)
+            answer += lowercase;
+
+        if (upperchecked)
+            answer += uppercase;
+
+        if (specialchecked)
+            answer += special;
+
+        return answer;
+    }
+
+    function buildPassword(string, quantity) {
+        let answer = "";
+        for (let i = 0; i < quantity; i++) {
+            //Returns a random integer from 0 to string.length
+            const num = Math.floor(Math.random() * string.length);
+            const char = string[num];
+            answer += char;
+        }
+        return answer;
+    }
+
+
+
+
+
+
+
+
+
+});
